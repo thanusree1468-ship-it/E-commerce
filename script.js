f5db5236a397e518da6ed905d3568c9f67c3c20a
thanusree1468-ship-it <thanusree1468@gmail.com>
2025-12-02 11:45:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// Add item
+function addToCart(name, price) {
+      let user = localStorage.getItem("loginUser");
+
+      if(!user) {
+        alert("Please login first!");
+        window.location.href = "login.html";
+        return;
+      }
+
+    cart.push({ name, price });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added!");
+}
+
+// Show items in cart
+function showCart() {
+    let box = document.getElementById("cart-items");
+    if (!box) return;
+
+    let total = 0;
+    box.innerHTML = "";
+
+    cart.forEach((item, index) => {
+        box.innerHTML += `
+                ${item.name} - $${item.price}
+                <button onclick="removeFromCart(${index})">Remove</button>
+            </p>
+        `;
+        total += item.price;
+    });
+
+    document.getElementById("total").innerText = "Total: $" + total;
+}
+
+// Remove item
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    showCart();
+}
+
+showCart();
+
+
+script.js
